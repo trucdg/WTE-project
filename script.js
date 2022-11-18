34b76d5365c88d0c2296a711c8f37bd8e4a5291e
Truc Duong <trucdg@bu.edu>
2022-11-18 10:52:48 -0500

    add a div for typeIt and add typeIt JS

--- script.js
+++ script.js
@@ -2,3 +2,31 @@
 function imgSlider(link) {
     document.querySelector('.food').src = link;
 }
+
+// TypeIt script
+new TypeIt("#typeIt-Element", {
+    speed: 80,
+    startDelay: 900,
+})
+    .type('Feeling <strong>HANGRY?</strong>', { delay: 150 })
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .delete(1)
+    .pause(200)
+    .type('<strong>HUNGRY?</strong>', { delay: 150 })
+    .break()
+    .pause()
+    .type('Which dish below do you like best?', {
+        speed: 100,
+    })
+    .go();
