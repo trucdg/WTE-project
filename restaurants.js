79f43bb5a1795b39e6bf7d04435e2ab64cd8c811
Truc Duong <trucdg@bu.edu>
2022-11-18 20:00:59 -0500

    use browser geolocation to get user's location, if geolocation is not supported or access to user's location is denied, use default location

--- restaurants.js
+++ restaurants.js
@@ -19,7 +19,7 @@ function initMap() {
 
     const icon = "https://img.icons8.com/plasticine/100/000000/user-location.png";
 
-    // create Marker function
+    //createMarker function, which takes in 2 inputs: (location, map)
 
     function createMarker(location, map) {
         let marker = new google.maps.Marker({
@@ -29,7 +29,36 @@ function initMap() {
         });
     }
 
+    // create a marker centered at BU
     createMarker(location, map);
+
+    // get location of the use using the user's browser geolocation
+    if (navigator.geolocation) {
+        console.log("Geolocation is supported!");
+
+        navigator.geolocation.getCurrentPosition(
+            (currentPosition) => {
+                location.lat = currentPosition.coords.latitude;
+                location.lng = currentPosition.coords.longitude;
+                map = new google.maps.Map(mapContainer, mapOptions);
+                createMarker(location, map);
+            },
+
+            (err) => {
+                console.log("Access to Geolocation is denied!");
+                console.log("Map is centered at default location.");
+                console.log(err.message);
+                map = new google.maps.Map(mapContainer, mapOptions);
+                createMarker(location, map);
+            }
+        );
+    } else {
+        // geolocation is not supported
+        console.log("Geolocation is not supported by the browser!");
+        console.log("Map is centered at the default location");
+        map = new google.maps.Map(mapContainer, mapOptions);
+        createMarker(location, map);
+    }
 }
 
 window.initMap = initMap;
