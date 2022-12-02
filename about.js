7a3250f017b9c6cb704a3548300225711adf9e62
Truc Duong <trucdg@bu.edu>
2022-12-02 01:40:31 -0500

    add js to move meh-btn feedback

---
+++ about.js
@@ -0,0 +1,25 @@
+let a = 0;
+function mouseOver() {
+    if (a == 0) {
+        buttonMoveRight();
+        a = 1;
+    } else {
+        buttonMoveLeft();
+        a = 0;
+    }
+}
+
+function buttonMoveRight() {
+    const button = document.getElementById('meh-btn');
+    button.style.transform = 'translateX(160%)';
+};
+
+function buttonMoveLeft() {
+    const button = document.getElementById('meh-btn');
+    button.style.transform = 'translateX(0)';
+}
+
+function buttonReset() {
+    const button = document.getElementById('meh-btn');
+    button.style.transform = 'translateX(0)';
+}
